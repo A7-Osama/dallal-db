49313de3fbd953891e0c4330889a5472a73f0a7a
A7-Osama <ao699168@gmail.com>
2026-03-02 21:54:24 +0300

    garbage has been gotten rid of

--- admin/config/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "Forbidden <br>
-You don't have permission to access to the server !!";
-
-
-
-
-
-
--- api-app/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Forbidden <br>
-You don't have permission to access to the server !!";
